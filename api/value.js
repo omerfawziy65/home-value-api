19043b26063080031a8c3cbe39f2e1585bab1bb4
omerfawziy65 <omerfawziy@yahoo.com>
2026-02-17 17:49:27 +0300

    Update value.js

--- api/value.js
+++ api/value.js
@@ -1,3 +1,11 @@
+res.setHeader("Access-Control-Allow-Origin", "*")
+res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS")
+res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+if (req.method === "OPTIONS") {
+  return res.status(200).end()
+}
+
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
